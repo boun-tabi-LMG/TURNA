b9e604fd52534e04310c799032b0418365c0448c
Onur Güngör <onur.gungor@udemy.com>
2023-09-19 13:39:48 +0300

    Fixing vocabulary task and data mismatch

--- start_train.sh
+++ start_train.sh
@@ -8,7 +8,7 @@ GCS_BUCKET="gs://turkish-llm-data"
 PROJECT_DIR_NAME="turkish-llm"
 PROJECT_DIR="${HOME}/${PROJECT_DIR_NAME}"
 T5X_DIR=${HOME}"/t5x"  # directory where the t5x is cloned.
-MODEL_DIR="${GCS_BUCKET}/${MODEL_NAME}" # maybe we should change this. things get messy when we use the same directory for both model and project.
+MODEL_DIR="${GCS_BUCKET}/models/${MODEL_NAME}" # maybe we should change this. things get messy when we use the same directory for both model and project.
 export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
