63df5a215295075233705ec71d88d6931be212a8
Onur Güngör <onur.gungor@udemy.com>
2023-09-23 23:29:55 +0300

    Issue #4: removing nohup

--- training_preparation/try_several_training_confs.sh
+++ training_preparation/try_several_training_confs.sh
@@ -11,11 +11,11 @@ for model_conf_prefix in ${model_conf_prefixes}; do
     for batch_size in ${batch_sizes}; do
         for input_length in ${input_lengths}; do
             echo "model_conf_prefix: ${model_conf_prefix}, batch_size: ${batch_size}, input_length: ${input_length}"
-            nohup bash ./start_train.sh ${model_conf_prefix}_pretrain.gin \
+            bash ./start_train.sh ${model_conf_prefix}_pretrain.gin \
                 ${model_conf_prefix}-bs_${batch_size}-il_${input_length}-${date_identifier} \
                 --gin.BATCH_SIZE=\"${batch_size}\" \
                 --gin.TASK_FEATURE_LENGTHS=\"\{\"inputs\": ${input_length}, \"targets\": ${input_length}\}\" \
-                >> train-${model_conf_prefix}-bs_${batch_size}-il_${input_length}-${date_identifier}.log & 
+                >> train-${model_conf_prefix}-bs_${batch_size}-il_${input_length}-${date_identifier}.log
         done
     done
 done
