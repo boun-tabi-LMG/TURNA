dc602aad7cce8e0230b72c70509770e69261e8f8
Onur Güngör <onur.gungor@udemy.com>
2023-09-24 12:33:45 +0300

    Issue #4: bugfix

--- training_preparation/try_several_training_confs.sh
+++ training_preparation/try_several_training_confs.sh
@@ -14,11 +14,11 @@ for model_conf_prefix in ${model_conf_prefixes}; do
             echo "model_conf_prefix: ${model_conf_prefix}, batch_size: ${batch_size}, input_length: ${input_length}"
             bash ./start_train.sh ${model_conf_prefix}_pretrain.gin \
                 ${model_conf_prefix}-bs_${batch_size}-il_${input_length}-${date_identifier} \
-                --gin.BATCH_SIZE=\"${batch_size}\" \
-                --gin.TASK_FEATURE_LENGTHS=\"\{\"inputs\": ${input_length}, \"targets\": ${input_length}\}\" \
+                --gin.BATCH_SIZE=${batch_size} \
+                --gin.TASK_FEATURE_LENGTHS="{'inputs': ${input_length}, 'targets': ${input_length}}" \
                 --gin.TRAIN_STEPS=\"${train_steps}\" \
-                --gin_bindings=\"train_script.train.eval_period=500\" \
-                --gin_bindings=\"utils.SaveCheckpointConfig.period=1000\" \
+                --gin.train_script.train.eval_period=500 \
+                --gin.utils.SaveCheckpointConfig.period=1000 \
                 >> train-${model_conf_prefix}-bs_${batch_size}-il_${input_length}-ts_${train_steps}-${date_identifier}.log 2>&1
         done
     done
