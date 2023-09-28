f0960c51b979597be32c0864cf1a61fb8d1f7e61
Onur Güngör <onur.gungor@udemy.com>
2023-09-28 22:45:10 +0300

    initial files

---
+++ dataset_preparation/oscarmc4_cleaned_hf_dataset/oscarmc4_cleaned_hf_dataset_tfds_dataset_builder_test.py
@@ -0,0 +1,25 @@
+"""oscarmc4_cleaned_hf_dataset_subset_combined_tfds dataset."""
+
+from . import oscarmc4_cleaned_hf_dataset_tfds_dataset_builder
+import tensorflow_datasets as tfds
+
+
+class Oscarmc4CleanedHfDatasetTfdsTest(tfds.testing.DatasetBuilderTestCase):
+    """Tests for oscarmc4_cleaned_hf_dataset_tfds dataset."""
+
+    # TODO(oscarmc4_cleaned_hf_dataset_tfds):
+    DATASET_CLASS = oscarmc4_cleaned_hf_dataset_tfds_dataset_builder.Builder
+    SPLITS = {
+        "train": 3,  # Number of fake train example
+        "test": 1,  # Number of fake test example
+    }
+
+    # If you are calling `download/download_and_extract` with a dict, like:
+    #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
+    # then the tests needs to provide the fake output paths relative to the
+    # fake data directory
+    # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+
+
+if __name__ == "__main__":
+    tfds.testing.test_main()
