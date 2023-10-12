cb9afc69f54ce1833c9e1dc500c09964c7821342
Onur Gungor <onurgu@gmail.com>
2023-10-12 22:17:56 +0300

    WIP #24

--- task_utils/ul2_objective.py
+++ task_utils/ul2_objective.py
@@ -1,25 +1,27 @@
+"""Contains the UL2 objective for pre-training."""
 import functools
 import tensorflow as tf
 import seqio
 import t5.data
 from typing import Optional, Sequence
 
+
 # found this function and modified from https://github.com/GoogleCloudPlatform/t5x-on-vertex-ai/blob/main/tasks/custom_tasks.py#L78
 # UL2 paper appendix code missed this function
-def prepend_prompt(dataset: tf.data.Dataset,
-                   output_features: seqio.preprocessors.OutputFeaturesType,
-                   sequence_length: Optional[
-                       seqio.preprocessors.SequenceLengthType] = None,
-                   prompt_mode: str = "",
-                   key: str = "inputs",
-                   mode: str = "") -> tf.data.Dataset:
+def prepend_prompt(
+    dataset: tf.data.Dataset,
+    output_features: seqio.preprocessors.OutputFeaturesType,
+    sequence_length: Optional[seqio.preprocessors.SequenceLengthType] = None,
+    prompt_mode: str = "",
+    key: str = "inputs",
+    mode: str = "",
+) -> tf.data.Dataset:
     """Prepends a prompt at the beginning of an input sequence."""
     del sequence_length
     if prompt_mode and mode:
         # output_features may not have inputs key
         out_keys = list(output_features.keys())
-        prompt_tokens = output_features[out_keys[0]
-                                        ].vocabulary.encode_tf(prompt_mode)
+        prompt_tokens = output_features[out_keys[0]].vocabulary.encode_tf(prompt_mode)
 
         def add_to_inputs(x):
             x[key] = tf.concat([prompt_tokens, x[key]], axis=0)
@@ -28,26 +30,31 @@ def prepend_prompt(dataset: tf.data.Dataset,
         dataset = dataset.map(add_to_inputs)
     return dataset
 
+
 # modified from t5.data.preprocessors because output_features may not have inputs key
-def split_tokens_to_inputs_length(dataset, sequence_length,
-                                  output_features, **kwargs):
-    max_tokens = sequence_length['inputs']
+def split_tokens_to_inputs_length(dataset, sequence_length, output_features, **kwargs):
+    max_tokens = sequence_length["inputs"]
     # output_features may not have inputs key
     out_keys = list(output_features.keys())
     if output_features[out_keys[0]].add_eos:
         # Leave room to insert an EOS token.
         max_tokens -= 1
 
-    return t5.data.preprocessors.split_tokens(dataset, max_tokens_per_segment=max_tokens, **kwargs)
+    return t5.data.preprocessors.split_tokens(
+        dataset, max_tokens_per_segment=max_tokens, **kwargs
+    )
+
 
 # modified from t5.data.preprocessors because output_features may not have inputs key
 def prefix_lm(dataset, sequence_length, output_features):
     """Prefix language modeling objective used in Raffel et al. 2019."""
     ds = dataset
-    ds = t5.data.preprocessors.select_random_chunk(ds, output_features=output_features,
-                                                   feature_key='targets', max_length=65536)
-    ds = split_tokens_to_inputs_length(ds, output_features=output_features,
-                                       sequence_length=sequence_length)
+    ds = t5.data.preprocessors.select_random_chunk(
+        ds, output_features=output_features, feature_key="targets", max_length=65536
+    )
+    ds = split_tokens_to_inputs_length(
+        ds, output_features=output_features, sequence_length=sequence_length
+    )
     ds = t5.data.preprocessors.denoise(
         ds,
         output_features,
@@ -58,21 +65,24 @@ def prefix_lm(dataset, sequence_length, output_features):
     )
     return ds
 
+
 # copied from UL2 paper https://arxiv.org/pdf/2205.05131.pdf appendix chapter 9.2
 # note: modified to use the prefix_lm() from above instead of the default t5.data.preprocessors.prefix_lm() because output_features may not have inputs key
-def ul2_objective(dataset: tf.data.Dataset,
-                  sequence_length: seqio.preprocessors.SequenceLengthType,
-                  output_features: seqio.preprocessors.OutputFeaturesType,
-                  use_prefix_lm_task: bool = False,
-                  rates: Optional[Sequence[float]] = None,
-                  mean_noise_span_lengths: Sequence[float] = (3.0,),
-                  noise_densities: Sequence[float] = (0.15,),
-                  shard_ds: bool = True,
-                  optional_task_prefixes: Optional[Sequence[str]] = None,
-                  input_feature_key: str = "inputs",
-                  merge_examples_to_reduce_padding: bool = True,
-                  reserved_for_packing: bool = None,
-                  seed: int = 7) -> tf.data.Dataset:
+def ul2_objective(
+    dataset: tf.data.Dataset,
+    sequence_length: seqio.preprocessors.SequenceLengthType,
+    output_features: seqio.preprocessors.OutputFeaturesType,
+    use_prefix_lm_task: bool = False,
+    rates: Optional[Sequence[float]] = None,
+    mean_noise_span_lengths: Sequence[float] = (3.0,),
+    noise_densities: Sequence[float] = (0.15,),
+    shard_ds: bool = True,
+    optional_task_prefixes: Optional[Sequence[str]] = None,
+    input_feature_key: str = "inputs",
+    merge_examples_to_reduce_padding: bool = True,
+    reserved_for_packing: bool = None,
+    seed: int = 7,
+) -> tf.data.Dataset:
     """UL2-like pre-training objectives.
     This preprocessor amounts to calling the 'span_corruption' function several
     times with different values of 'noise_density' and 'mean_noise_span_length'.
@@ -100,8 +110,7 @@ def ul2_objective(dataset: tf.data.Dataset,
         num_tasks = len(noise_densities) + int(use_prefix_lm_task)
         valid_number_of_prefixes = num_tasks == len(optional_task_prefixes)
         if not valid_number_of_prefixes:
-            raise ValueError(
-                "Number of task prefixes must match number of tasks.")
+            raise ValueError("Number of task prefixes must match number of tasks.")
     inputs_length = sequence_length[input_feature_key]
     input_lengths, targets_lengths = [], []
     sequence_lengths = {x: y for x, y in sequence_length.items()}
@@ -117,27 +126,28 @@ def ul2_objective(dataset: tf.data.Dataset,
             inputs_length=inputs_length,
             mean_noise_span_length=mean_noise_span_length,
             noise_density=noise_density,
-            verbose=True)
+            verbose=True,
+        )
         input_lengths.append(input_length)
         targets_lengths.append(targets_length)
 
         if sequence_length["targets"] < targets_length:
             upper_bound = max(targets_lengths)
             raise ValueError(
-                f'Expected max targets length for span corruption ({upper_bound}) is '
-                f'greater than configured targets length '
-                f"({sequence_length['targets']})")
+                f"Expected max targets length for span corruption ({upper_bound}) is "
+                f"greater than configured targets length "
+                f"({sequence_length['targets']})"
+            )
     print("input_lengths: ", input_lengths)
     print("targets_lengths: ", targets_lengths)
     ds = dataset
     ds = t5.data.preprocessors.select_random_chunk(
-        ds,
-        output_features=output_features,
-        feature_key="targets",
-        max_length=65536)
+        ds, output_features=output_features, feature_key="targets", max_length=65536
+    )
     if merge_examples_to_reduce_padding:
         ds = t5.data.preprocessors.reduce_concat_tokens(
-            ds, feature_key="targets", batch_size=128)
+            ds, feature_key="targets", batch_size=128
+        )
     num_shards = len(input_lengths) + int(use_prefix_lm_task)
     if shard_ds:
         ds_shards = [ds.shard(num_shards, i) for i in range(num_shards)]
@@ -151,7 +161,8 @@ def ul2_objective(dataset: tf.data.Dataset,
             ds,
             feature_key="targets",
             min_tokens_per_segment=None,
-            max_tokens_per_segment=input_length)
+            max_tokens_per_segment=input_length,
+        )
         ds = t5.data.preprocessors.denoise(
             ds,
             output_features,
@@ -160,27 +171,30 @@ def ul2_objective(dataset: tf.data.Dataset,
             noise_density=noise_density,
             noise_mask_fn=functools.partial(
                 t5.data.preprocessors.random_spans_noise_mask,
-                mean_noise_span_length=noise_span_length),
-            input_feature_key=input_feature_key)
+                mean_noise_span_length=noise_span_length,
+            ),
+            input_feature_key=input_feature_key,
+        )
         if optional_task_prefixes:
             ds = prepend_prompt(
                 ds,
                 output_features,
                 prompt_mode=optional_task_prefixes[i],
                 mode=optional_task_prefixes[i],
-                key=input_feature_key)
+                key=input_feature_key,
+            )
         processed_ds.append(ds)
     if use_prefix_lm_task:
         ds = ds_shards[-1]
-        ds = prefix_lm(
-            ds, sequence_lengths, output_features)
+        ds = prefix_lm(ds, sequence_lengths, output_features)
         if optional_task_prefixes:
             ds = prepend_prompt(
                 ds,
                 output_features,
                 prompt_mode=optional_task_prefixes[-1],
                 mode=optional_task_prefixes[-1],
-                key=input_feature_key)
+                key=input_feature_key,
+            )
         processed_ds.append(ds)
     ds = tf.data.experimental.sample_from_datasets(processed_ds, rates, seed)
     return ds
--- tasks.py
+++ tasks.py
@@ -1,13 +1,12 @@
 import functools
+
 import seqio
-import tensorflow as tf
 import t5.data
 from t5.data import postprocessors
 from t5.data import preprocessors
 from t5.evaluation import metrics
-from seqio import FunctionDataSource, utils
 
-from ul2_objective import ul2_objective
+from task_utils.ul2_objective import ul2_objective
 
 # values from UL2 paper https://arxiv.org/pdf/2205.05131.pdf chapter 3.1.2 table 1
 R_DENOISER_SPAN_LENGTHS = [3.0, 8.0]
@@ -20,6 +19,7 @@ X_DENOISER_TOKEN_PREFIX = "[NLG]"
 S_DENOISER_TOKEN_PREFIX = "[S2S]"
 
 TaskRegistry = seqio.TaskRegistry
+MixtureRegistry = seqio.MixtureRegistry
 
 vocabulary = seqio.SentencePieceVocabulary(
     "SentencePiece_32k_Tokenizer-denoiser-tokens-added-02.model", extra_ids=100
@@ -30,38 +30,76 @@ DEFAULT_OUTPUT_FEATURES = {
     "targets": seqio.Feature(vocabulary=vocabulary, add_eos=True),
 }
 
+dataset_gcs_url = "gs://turkish-llm-data/datasets"
 
-dataset_name = "oscarmc4_cleaned_hf_dataset_subset_combined_tfds:1.0.0"
-dataset_gcs_url = "gs://turkish-llm-data/"
+dataset_names = [
+    ("bilkent_creative_writings", 16.0),
+    ("book_corpus_v2", 8.0),
+    ("dergipark", 6.0),
+    ("oscarmc4_cleaned_hf_dataset", 1.0),
+    ("parlamint_tr", 12.0),
+    ("yoktez", 2.0),
+]
 
-TaskRegistry.add(
-    "pretrain_turkish_ul2",
-    source=seqio.TfdsDataSource(tfds_name=dataset_name, tfds_data_dir=dataset_gcs_url),
-    preprocessors=[
-        functools.partial(
-            seqio.preprocessors.rekey, key_map={"inputs": None, "targets": "text"}
+dataset_versions = ["1.0.0" for _ in range(len(dataset_names))]
+
+preprocessing_pipeline = [
+    functools.partial(
+        seqio.preprocessors.rekey, key_map={"inputs": None, "targets": "text"}
+    ),
+    seqio.preprocessors.tokenize,
+    functools.partial(
+        ul2_objective,
+        shard_ds=False,
+        use_prefix_lm_task=True,  # use S-denoising
+        # equal total 40% rate for both R- and X-denoisers + 20% for S-denoising
+        # (suggested at the paper chapter 4.5)
+        rates=[0.4 / len(R_DENOISER_SPAN_LENGTHS)] * len(R_DENOISER_SPAN_LENGTHS)
+        + [0.4 / len(X_DENOISER_SPAN_LENGTHS)] * len(X_DENOISER_SPAN_LENGTHS)
+        + [0.2],
+        mean_noise_span_lengths=R_DENOISER_SPAN_LENGTHS + X_DENOISER_SPAN_LENGTHS,
+        noise_densities=R_DENOISER_CORRUPT_RATES + X_DENOISER_CORRUPT_RATES,
+        optional_task_prefixes=[R_DENOISER_TOKEN_PREFIX] * len(R_DENOISER_SPAN_LENGTHS)
+        + [X_DENOISER_TOKEN_PREFIX] * len(X_DENOISER_SPAN_LENGTHS)
+        + [S_DENOISER_TOKEN_PREFIX],
+        reserved_for_packing=1,  # make room for task prefix token
+    ),
+    seqio.preprocessors.append_eos_after_trim,
+]
+
+for dataset_name, version in zip(dataset_names, dataset_versions):
+    TaskRegistry.add(
+        f"pretrain_{dataset_name}",
+        source=seqio.TfdsDataSource(
+            tfds_name=dataset_name, tfds_data_dir=dataset_gcs_url
         ),
-        seqio.preprocessors.tokenize,
-        functools.partial(
-            ul2_objective,
-            shard_ds=False,
-            use_prefix_lm_task=True,  # use S-denoising
-            rates=[0.4 / len(R_DENOISER_SPAN_LENGTHS)] * len(R_DENOISER_SPAN_LENGTHS)
-            + [0.4 / len(X_DENOISER_SPAN_LENGTHS)] * len(X_DENOISER_SPAN_LENGTHS)
-            + [
-                0.2
-            ],  # equal total 40% rate for both R- and X-denoisers + 20% for S-denoising (suggested at the paper chapter 4.5)
-            mean_noise_span_lengths=R_DENOISER_SPAN_LENGTHS + X_DENOISER_SPAN_LENGTHS,
-            noise_densities=R_DENOISER_CORRUPT_RATES + X_DENOISER_CORRUPT_RATES,
-            optional_task_prefixes=[R_DENOISER_TOKEN_PREFIX]
-            * len(R_DENOISER_SPAN_LENGTHS)
-            + [X_DENOISER_TOKEN_PREFIX] * len(X_DENOISER_SPAN_LENGTHS)
-            + [S_DENOISER_TOKEN_PREFIX],
-            reserved_for_packing=1,  # make room for task prefix token
+        preprocessors=preprocessing_pipeline,
+        output_features=DEFAULT_OUTPUT_FEATURES,
+        metric_fns=[metrics.accuracy],
+    )
+
+    TaskRegistry.add(
+        f"count_{dataset_name}",
+        source=seqio.TfdsDataSource(
+            tfds_name=dataset_name, tfds_data_dir=dataset_gcs_url
         ),
-        seqio.preprocessors.append_eos_after_trim,
-    ],
-    # output_features={"targets": DEFAULT_OUTPUT_FEATURES["targets"]},
-    output_features=DEFAULT_OUTPUT_FEATURES,
-    metric_fns=[metrics.accuracy],
+        preprocessors=[
+            functools.partial(
+                seqio.preprocessors.tokenize,
+                output_features={
+                    "text": seqio.Feature(
+                        vocabulary=vocabulary, add_eos=False, required=False
+                    )
+                },
+            )
+        ],
+        output_features={
+            "text": seqio.Feature(vocabulary=vocabulary, add_eos=False, required=False)
+        },
+    )
+
+MixtureRegistry.add(
+    "pretrain_all",
+    [(f"pretrain_{dataset_name}", rate) for dataset_name, rate in dataset_names],
+    default_rate=1.0,
 )
