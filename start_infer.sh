6ad26571a384f255cb73bd985bd7c6bae0a1b307
Onur Gungor <onurgu@gmail.com>
2023-11-07 22:12:56 +0300

    bugfix: start_infer.sh

--- start_infer.sh
+++ start_infer.sh
@@ -15,12 +15,6 @@ T5X_DIR=${HOME}"/t5x"  # directory where the t5x is cloned.
 MODEL_DIR="${GCS_BUCKET}/models/${MODEL_NAME}" # maybe we should change this. things get messy when we use the same directory for both model and project.
 export PYTHONPATH=${PROJECT_DIR}
 
-python3 ${T5X_DIR}/t5x/train.py \
-  --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="${PRETRAIN_GIN_FILEPATH}" \
-  --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  ${ADDITIONAL_GIN_PARAMS}
-
 python3 ${T5X_DIR}/t5x/infer.py \
   --gin_search_paths=${PROJECT_DIR} \
   --gin_file="${PRETRAIN_GIN_FILEPATH}" \
