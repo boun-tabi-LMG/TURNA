a02c0bbfd860cbf2197a9e31edb06ae6e5904da5
Onur Gungor <onurgu@gmail.com>
2023-11-22 18:18:57 +0300

    env activation and others: watcher.sh

--- training_tracking/watcher.sh
+++ training_tracking/watcher.sh
@@ -2,15 +2,15 @@
 #!/bin/bash
 
 REPO_DIR=$(dirname "$0")/..
-TPU_NAME=$(1:-tpu-vm-08)
+TPU_NAME=${1:-tpu-vm-08}
 WAIT_TIME=1800
 ITERATION=0
 
-pyenv activate turkish-llm
+source /home/onur.gungor/.pyenv/versions/turkish-llm/bin/activate
 
 while true; do
     cd ${REPO_DIR} && \
-        poetry run python turkish-llm/training_tracking/watch_training.py --tpu_name ${TPU_NAME}
+        python training_tracking/watch_training.py --tpu_name ${TPU_NAME}
     
     sleep ${WAIT_TIME}  # Wait for 30 minutes (1800 seconds)
     ITERATION=$((ITERATION + 1))  # Increment the ITERATION variable by 1
@@ -18,7 +18,7 @@ while true; do
     echo "Iteration: ${ITERATION}"
     if [[ ${MODULO} -eq 0 ]]; then
         cd ${REPO_DIR} && \
-        poetry run python turkish-llm/training_tracking/watch_training.py --tpu_name ${TPU_NAME} --send_report
+        python training_tracking/watch_training.py --tpu_name ${TPU_NAME} --send_report
     fi
     
 done
