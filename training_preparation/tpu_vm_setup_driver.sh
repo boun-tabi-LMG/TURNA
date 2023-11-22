5c3ac84d12b9f2842827f6def24281ef2ec14661
Onur Gungor <onurgu@gmail.com>
2023-11-22 17:10:50 +0300

    adding preemptible directive in the tpu create command

--- training_preparation/tpu_vm_setup_driver.sh
+++ training_preparation/tpu_vm_setup_driver.sh
@@ -6,6 +6,7 @@
 #   --zone=europe-west4-a \
 #   --accelerator-type=v4-8 \
 #   --version=tpu-vm-tf-2.13.0
+#   --preemptible
 
 TPU_VM_NAME=${1:-tpu-vm-05}
 
@@ -22,5 +23,5 @@ bash ${TRAINING_PREPARATION_DIR}/tpu_vm_cp_code.sh ${REPO_DIR} ${TPU_VM_NAME} ${
 gcloud compute tpus tpu-vm scp ${TRAINING_PREPARATION_DIR}/tpu_vm_setup.sh ${TPU_VM_NAME}: && \
 gcloud compute tpus tpu-vm ssh ${TPU_VM_NAME} -- "bash ./tpu_vm_setup.sh"
 
-gcloud compute tpus tpu-vm scp vocabulary_preparation/SentencePiece_32k_Tokenizer-denoiser-tokens-added-02.model \
+gcloud compute tpus tpu-vm scp ${REPO_DIR}/vocabulary_preparation/SentencePiece_32k_Tokenizer-denoiser-tokens-added-02.model \
 ${TPU_VM_NAME}:~/turkish-llm/SentencePiece_32k_Tokenizer-denoiser-tokens-added-02.model
