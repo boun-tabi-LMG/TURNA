866c40f9a443729b26a763baf7e2ba9657bc0215
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-22 23:31:04 +0300

    Move experiment utils to a directory

--- training_preparation/experiment_utils/start_train_helper.sh
+++ training_preparation/experiment_utils/start_train_helper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-model_conf_prefixes='large large_nl36 base_nl36'
+model_conf_prefixes='tiny'
 
-batch_sizes='64 128 256'
-input_lengths='512 1024 2048'
-train_steps='1000'
+batch_sizes='48'
+input_lengths='512'
+train_steps='1000000'
+task_name='pretrain_all'
 
 date_identifier=$(date +%Y%m%d_%H%M%S)
 
@@ -12,13 +13,14 @@ for model_conf_prefix in ${model_conf_prefixes}; do
     for batch_size in ${batch_sizes}; do
         for input_length in ${input_lengths}; do
             echo "model_conf_prefix: ${model_conf_prefix}, batch_size: ${batch_size}, input_length: ${input_length}"
-            bash ./start_train.sh ${model_conf_prefix}_pretrain.gin \
+            bash ./start_train.sh gins/${model_conf_prefix}_pretrain.gin \
                 ${model_conf_prefix}-bs_${batch_size}-il_${input_length}-${date_identifier} \
+                --gin.MIXTURE_OR_TASK_NAME=\"${task_name}\" \
                 --gin.BATCH_SIZE=${batch_size} \
                 --gin.TASK_FEATURE_LENGTHS=\{\'inputs\':${input_length},\'targets\':${input_length}\} \
                 --gin.TRAIN_STEPS=${train_steps} \
-                --gin.train.eval_period=500 \
-                --gin.utils.SaveCheckpointConfig.period=1000 \
+                --gin.train.eval_period=10000 \
+                --gin.utils.SaveCheckpointConfig.period=10000 \
                 >> train-${model_conf_prefix}-bs_${batch_size}-il_${input_length}-ts_${train_steps}-${date_identifier}.log 2>&1
         done
     done
--- training_preparation/start_train_on_tpu.sh
+++
@@ -1,4 +0,0 @@
-
-TPU_VM_NAME=${1}
-
-gcloud compute tpus tpu-vm ssh ${TPU_VM_NAME} -- "cd ~/turkish-llm && nohup bash ./start_train.sh large_nl36_bs48_pretrain_all.gin large_nl36-bs_48-il_512-20231013_043959 >> train-large_nl36-bs_48-il_512-20231013_043959-01.log &"
