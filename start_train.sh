b86a0f367d6b50d60cbfa77121b20d0b031c9da9
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-23 21:15:19 +0300

    Delete extra / in start_train.sh

--- start_train.sh
+++ start_train.sh
@@ -9,7 +9,7 @@ ADDITIONAL_GIN_PARAMS="${@:3}"
 
 GCS_BUCKET="gs://turkish-llm-data"
 PROJECT_DIR_NAME="turkish-llm"
-PROJECT_DIR="${HOME}/${PROJECT_DIR_NAME}/"
+PROJECT_DIR="${HOME}/${PROJECT_DIR_NAME}"
 T5X_DIR=${HOME}"/t5x"  # directory where the t5x is cloned.
 MODEL_DIR="${GCS_BUCKET}/models/${MODEL_NAME}" # maybe we should change this. things get messy when we use the same directory for both model and project.
 export PYTHONPATH=${PROJECT_DIR}
