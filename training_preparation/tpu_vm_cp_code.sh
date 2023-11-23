5e869d4d50076a32f1ab88d035ef4e09a9d510a0
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-11-23 09:51:39 +0300

    Fix tpu_vm_cp_code

--- training_preparation/tpu_vm_cp_code.sh
+++ training_preparation/tpu_vm_cp_code.sh
@@ -2,10 +2,10 @@
 REPO_DIR=${1}
 TPU_VM_NAME=${2}
 TRAINING_PREPARATION_DIR=${3}
-TMP_CODE_ARCHIVE_NAME=tmp-turkish-llm.tar.gz
+TMP_CODE_ARCHIVE_NAME=${4:-tmp-turkish-llm.tar.gz}
 
 cd ${REPO_DIR} && \
-git archive main -o ${4} && \
+git archive main -o ${TMP_CODE_ARCHIVE_NAME} && \
 gcloud compute tpus tpu-vm scp \
   ${4} \
   ${TRAINING_PREPARATION_DIR}/tpu_vm_setup_extract_the_code_archive.sh \
